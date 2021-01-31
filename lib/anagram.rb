015b4d505c35b5818cf2518f126ce75ebe6d3bb6
Robert Hardy <rwhj77@hotmail.com>
2021-01-31 15:44:21 -0600

    commit tree d3a90c639280f74cf85ce35285e068b0a9608ab5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word, :reverse_array
+
+    def initialize(word)
+        self.word = word.downcase
+        self.reverse_array = self.word.reverse.split("").sort
+    end
+
+    def match(words_array)
+        result = []
+        words_array.each do |candidate|
+            if candidate.split("").sort == self.reverse_array
+                result << candidate
+            end
+        end
+        return result
+    end
+end
+
+
